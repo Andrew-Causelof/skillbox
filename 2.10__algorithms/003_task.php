e9e6f9b51ed2d73606b7e206b4c9ba07d373de29
Andrey <44496797+Andrew-Causelof@users.noreply.github.com>
2021-01-18 16:29:26 +0300

    non impotant changes

--- 2.10__algorithms/003_task.php
+++ 2.10__algorithms/003_task.php
@@ -33,7 +33,7 @@ foreach ($values as $key => $value) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Перестановка букв в слове (циклический сдвиг влево)</title>
+    <title>Задание 3 - Поиск min элемента в массиве</title>
         
     <link rel="stylesheet" href="style.css">
 </head>
